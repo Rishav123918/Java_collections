4559d49cb05b9c2819e1d94dff2bd982ad74f1f0
Rishav kumar jain <rishavkumarjain2000@gmail.com>
2025-07-30 23:13:34 +0530

    update

--- src/Singleton/main.java
+++ src/Singleton/main.java
@@ -1,8 +1,11 @@
 package Singleton;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String args[])
     {
+        Scanner scanner = new Scanner(System.in);
         Singleton_Lazy s1=Singleton_Lazy.getInstance();
         Singleton_Lazy s2=Singleton_Lazy.getInstance();
 
